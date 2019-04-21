3067c329d4f8ff4f412faf5bfe513c80c995d069
TammyLaforest <TammyLaforest@gmail.com>
2019-04-21 15:15:20 -0400

    learning node.js correctly. Hello World complete

---
+++ node-sample/hello-node/server.js
@@ -0,0 +1,14 @@
+let http = require('http')
+
+let port = 3000
+
+let requestHandler = (request, response) => {
+	console.log(`New request to ${request.url}`)
+	response.end(`Hello World!`)
+}
+
+let server = http.createServer(requestHandler)
+
+server.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
